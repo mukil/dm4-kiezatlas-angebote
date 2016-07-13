66f1d4e30bd637e75910307cee7198688b0073d0
Malte Reißig <malte@mikromedia.de>
2016-07-13 23:39:48 +0200

    Reduce migration2.

--- src/main/java/de/kiezatlas/angebote/migrations/Migration2.java
+++ src/main/java/de/kiezatlas/angebote/migrations/Migration2.java
@@ -1,8 +1,6 @@
 package de.kiezatlas.angebote.migrations;
 
 import de.deepamehta.core.Topic;
-import de.deepamehta.core.TopicType;
-import de.deepamehta.core.ViewConfiguration;
 import de.deepamehta.core.service.Migration;
 import de.deepamehta.core.service.accesscontrol.SharingMode;
 import de.deepamehta.core.service.Inject;
@@ -12,7 +10,7 @@ import java.util.logging.Logger;
 
 
 /**
- * This migration extends the dm4-kiezatlas "ka2.geo_object" type about a custom webclient multi renderer.
+ * Creates a new "Public" Workspace called "Angebote".
  *
  */
 public class Migration2 extends Migration {
@@ -25,15 +23,15 @@ public class Migration2 extends Migration {
     @Override
     public void run() {
 
-        TopicType tagType = dm4.getTopicType("ka2.geo_object");
+        /** TopicType tagType = dm4.getTopicType("ka2.geo_object");
         ViewConfiguration viewConfig = tagType.getViewConfig();
         //
         viewConfig.addSetting("dm4.webclient.view_config",
-                "dm4.webclient.multi_renderer_uri", "de.kiezatlas.angebote.geo_object_multi");
+                "dm4.webclient.multi_renderer_uri", "de.kiezatlas.angebote.geo_object_multi"); **/
         //
         Topic angeboteWorkspace = workspaceService.createWorkspace("Angebote", "de.kiezatlas.angebote_ws",
                 SharingMode.PUBLIC);
-        aclService.setWorkspaceOwner(angeboteWorkspace, "admin");
+        aclService.setWorkspaceOwner(angeboteWorkspace, AccessControlService.ADMIN_USERNAME);
 
     }
 
